c499b9a0092f8a67374963c3920b72bbd455d00c
Kendrick Bong <Kendrickbong1996@gmail.com>
2019-11-27 16:31:37 +0800

    gave up on coles, atleast browser loads woolies

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-async function getPic() {
-  console.log('Starting function');
-  const browser = await puppeteer.launch();
+async function scrape() {
+  const browser = await puppeteer.launch({
+    headless: false
+    // executablePath:
+    //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  });
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.screenshot({path: 'google.png'});
-
-  await browser.close();
-  console.log('Ending Function');
+  await page.goto(
+    `https://www.woolworths.com.au/shop/productdetails/694859/woolworths-frozen-meal-butter-chicken-rice`
+  );
+  await page.waitFor(5000);
+  const result = await page.evaluate(() => {
+    let title = document.querySelector(".productDetail-tileName").innerText;
+    let price = document.querySelector(
+      "#center-panel > div > wow-product-detail > div > wow-product-detail-tile > section > div > div.productDetail-tileContainer > div.productDetail-tile.has-header > div:nth-child(2) > div > div:nth-child(6) > wow-price > div"
+    ).innerText;
+    return {
+      title,
+      price
+    };
+  });
+  browser.close();
+  return result;
 }
 
-getPic();
+scrape().then(value => {
+  console.log(value); // Success!
+});
