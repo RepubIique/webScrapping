eb55196f5175bf517fe13cb75f2ef346b6fced01
Bryan Kho <bryank1991@hotmail.com>
2019-11-28 00:25:49 +0800

    added search function

--- app.js
+++ app.js
@@ -23,6 +23,34 @@ async function scrape() {
   return result;
 }
 
-scrape().then(value => {
+async function search() {
+  const browser = await puppeteer.launch({
+    headless: false
+    // executablePath:
+    //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  });
+  const page = await browser.newPage();
+  let searchString = combineArguments();
+  await page.goto(
+    "https://www.woolworths.com.au/shop/search/products?searchTerm=" + searchString
+  );
+  await page.waitFor(5000);
+  browser.close();
+  return 'success'
+}
+
+// scrape().then(value => {
+//   console.log(value); // Success!
+// });
+
+search().then(value => {
   console.log(value); // Success!
 });
+
+// HELPER FUNCTIONS
+function combineArguments() {
+  // https://stackoverflow.com/questions/38824479/concatting-arguments-after-argv2-nodejs/38824618
+  let arr = process.argv.splice(2, process.argv.length - 1);
+  return arr.join('%20');
+}
+
